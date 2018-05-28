219e1759326e048e7b4f7f856d67ecdd12fee0c3
Sasa Prsir <s.prsir@thepimcompany.com>
2018-05-28 12:40:47 +0200

    Comment fixed.

--- php/transposeArray.php
+++ php/transposeArray.php
@@ -51,19 +51,19 @@ var_dump(transpose($array));
 
 /**
 (array) [3 elements]
-1: 
-(array) [3 elements]
-a: (string) a1
-b: (string) b1
-c: (string) c1
-2: 
-(array) [3 elements]
-a: (string) a2
-b: (string) b2
-c: (string) c2
-3: 
-(array) [3 elements]
-a: (string) a3
-b: (string) b3
-c: (string) c3
+  1: 
+    (array) [3 elements]
+    a: (string) a1
+    b: (string) b1
+    c: (string) c1
+  2: 
+    (array) [3 elements]
+    a: (string) a2
+    b: (string) b2
+    c: (string) c2
+  3: 
+    (array) [3 elements]
+    a: (string) a3
+    b: (string) b3
+    c: (string) c3
 */
