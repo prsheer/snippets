da2384056b55e36a8354733afec9ec8ed8be9570
Sasa Prsir <s.prsir@thepimcompany.com>
2018-05-29 14:09:37 +0200

    Recursion added.

---
+++ php/recursion/recursionI.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Input:
+ * $json_str = '{
+    "one":
+    {
+        "two": 3,
+        "four": [ 5,6,7]
+    },
+    "eight":
+    {
+        "nine":
+        {
+            "ten":11
+        }
+    }
+}';
+
+Output:
+[
+    'one/two' => 3,
+    'one/four/0' => 5,
+    'one/four/1' => 6,
+    'one/four/2' => 7,
+    'eight/nine/ten' => 11
+]
+ */
+
+$json = '
+{
+    "one":
+    {
+        "two": 3,
+        "four": [ 5,6,7]
+    },
+    "eight":
+    {
+        "nine":
+        {
+            "ten":11
+        }
+    }
+}';
+
+$object = json_decode($json);
+$result = [];
+
+function convertJsonToSingleArray($object, &$result, $prefix = '') {
+
+    if (!is_array($object) && !is_object($object)) {
+        $prefix = rtrim($prefix, '/');
+        $result[] = $prefix . " => " . $object . "\n";
+
+        d($result);
+        return;
+    }
+
+    foreach ($object as $key => $value) {
+        $prefix .= is_int($key) ? ($key + 1) . "/" : $key . "/";
+        convertJsonToSingleArray($value, $result, $prefix);
+    }
+
+}
+
+convertJsonToSingleArray($object, $result);
+
+function d($v) {
+    return var_dump($v);
+}
+//d($result);
